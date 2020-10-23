bb441ebede1ef1e7c4ac82d18d0fda0eee2fc865
Francis Eudy <francis.eudy@gmail.com>
2020-10-22 22:41:18 -0400

    custom user api

---
+++ drfCustomUserAPI/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/reudy15/Documents/workspace/django_sandbox/drfCustomUserAPI/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
